7dc32abae1fdfde1db2fb78f08e2727a48b235ff
AlexIv <ivlieva.medphyslab.com>
2023-02-21 21:26:28 +0300

    To lay M numbers in the array  added

--- Zadacha41/Zadacha41.cs
+++ Zadacha41/Zadacha41.cs
@@ -3,3 +3,19 @@
 0, 7, 8, -2, -2 -> 2
 
 1, -7, 567, 89, 223-> 3*/
+System.Console.WriteLine("How many numbers you want to enter: "); 
+int M = Int32.Parse(System.Console.ReadLine());
+int [] ToGetMArray ()
+{
+    int [] MArray= new int [M];
+    for (int i=0; i<M; i++)
+    {
+     System.Console.WriteLine("Enter number: ");
+     MArray[i]=Int32.Parse(System.Console.ReadLine());
+    }
+    return MArray;
+}
+int ToCountPositiveNumbersInArray (int [] ToGetMArray)
+{
+    
+}
