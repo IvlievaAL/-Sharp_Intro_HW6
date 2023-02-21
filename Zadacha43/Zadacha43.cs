cfdf4fbd20de78e69c01ba852a66391e3c8dc734
AlexIv <ivlieva.medphyslab.com>
2023-02-21 20:06:13 +0300

    Types changed to double

--- Zadacha43/Zadacha43.cs
+++ Zadacha43/Zadacha43.cs
@@ -4,10 +4,11 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 System.Console.WriteLine("Enter b1: "); 
-int b1 = Int32.Parse(System.Console.ReadLine());
+double b1 = Double.Parse(System.Console.ReadLine());
 System.Console.WriteLine("Enter k1: "); 
-int k1 = Int32.Parse(System.Console.ReadLine());
+double k1 = Double.Parse(System.Console.ReadLine());
 System.Console.WriteLine("Enter b2: "); 
-int b2 = Int32.Parse(System.Console.ReadLine());
+double b2 = Double.Parse(System.Console.ReadLine());
 System.Console.WriteLine("Enter k2: "); 
-int k2 = Int32.Parse(System.Console.ReadLine());
+double k2 = Double.Parse(System.Console.ReadLine());
+int y
