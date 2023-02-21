4a9759ffc94979ed5f657c39ad29ad0ad179e273
AlexIv <ivlieva.medphyslab.com>
2023-02-21 20:53:52 +0300

    Code is written and works

--- Zadacha44/Zadacha44.cs
+++ Zadacha44/Zadacha44.cs
@@ -6,3 +6,36 @@
 --> 0 1 1
 Если N = 7
 --> 0 1 1 2 3 5 8*/
+System.Console.WriteLine("Enter N"); 
+int N = Int32.Parse(System.Console.ReadLine());
+MassiveAsString (Fibonachi(N));
+int[] Fibonachi (int N)
+{
+    int[] FibonachiArray = new int [N];
+    int N1=0;
+    int N2=1;
+    if (N==1)
+    {
+     FibonachiArray[0]=N1;
+    }
+    else if (N==2)
+    {
+        FibonachiArray[0]=N1;
+        FibonachiArray[1]=N2;
+    }
+    else if (N>2)
+    {
+        FibonachiArray[0]=N1;
+        FibonachiArray[1]=N2;
+        for (int i=2; i<=N-1;i++)
+        {
+          FibonachiArray[i]=FibonachiArray[i-1]+FibonachiArray[i-2];
+        }
+    }
+    return FibonachiArray;
+}
+void MassiveAsString (int [] FibonachiArray)
+{
+    string ArrayAsString = string.Join(" , ", FibonachiArray); 
+    System.Console.WriteLine(ArrayAsString);
+}
