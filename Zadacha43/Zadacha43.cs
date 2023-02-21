62e14e9cac330e81fd0d426b26ae41dc4ea83fb6
AlexIv <ivlieva.medphyslab.com>
2023-02-21 20:03:43 +0300

    Input added

--- Zadacha43/Zadacha43.cs
+++ Zadacha43/Zadacha43.cs
@@ -3,3 +3,11 @@
 значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+System.Console.WriteLine("Enter b1: "); 
+int b1 = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter k1: "); 
+int k1 = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter b2: "); 
+int b2 = Int32.Parse(System.Console.ReadLine());
+System.Console.WriteLine("Enter k2: "); 
+int k2 = Int32.Parse(System.Console.ReadLine());
